f949161cd3e65cab1beb9057eff3bb59e29b9e80
John Odom <joodom91@gmail.com>
2019-07-09 13:49:09 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,7 +7,7 @@ def run_guessing_game
     puts "You guessed the correct number!"
   elsif a == "exit"
     puts "Goodbye!"
-  elsif a != random_number
+  elsif a !== random_number
     puts "Sorry! The computer guessed #{random_number + 1}."
   end
 end
